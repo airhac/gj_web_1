c70f924ce81539ade896bed54b9302fa647d17a3
airhac <oooh32141@gmail.com>
2021-07-07 14:54:53 +0900

    7/7 web commit 1

--- gw_odh_project1/settings.py
+++ gw_odh_project1/settings.py
@@ -136,7 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#이거랑 다른곳에 base와 같이 연결 되어 href
 
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static/",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
