e9a9a3eeb4bdc3b4be969799ee534a236f4a34f7
airhac <oooh32141@gmail.com>
2021-07-09 09:34:05 +0900

    7/8 new commit 1

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#모델하나를 적는데 이것이 데이터베이스의 데이블 같은것이 된다.
+#상속을 받는다,
+#장고에서 기본으로 제공해주는 기초 모델
+class NewModel(models.Model):
+    text = models.CharField(max_length=225, null=False)
+
+#문자열을 입력받는 모델
