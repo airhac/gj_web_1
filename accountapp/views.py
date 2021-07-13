10e668a238e03b3a1eae271ac18c3a4f73bcb5de
airhac <oooh32141@gmail.com>
2021-07-13 10:17:16 +0900

    7/13 new commit 2 1. Post 방식으로 data_list로 보여주는 것을 마지막 요청이  get으로 바뀌도록 해준다.

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import NewModel
 
@@ -12,10 +13,8 @@ def hello_world(request):
         model_instance.text = temp#이 값을 저장하는 데이터를 호풀해야함
         model_instance.save() #모델에 데이터를 저장한다.
 
-        data_list = NewModel.objects.all() #이 모델의 모든 오브젝트드을 가지고 오겠다.
-
-
-        return render(request,'accountapp/hello_world.html', context={'data_list': data_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))#url을 nameing 해주는 것이다.
+        #마지막 요청이 get으로 바뀐다.
     else:
         data_list = NewModel.objects.all()  # 이 모델의 모든 오브젝트드을 가지고 오겠다.
 
