44fbeec83c28f388d48ba19f2809ca70c60e6c99
airhac <oooh32141@gmail.com>
2021-07-13 09:47:49 +0900

    7/13 new commit 2 1. list 읽어오기 2. get 방식으로도 해주기

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,7 +12,12 @@ def hello_world(request):
         model_instance.text = temp#이 값을 저장하는 데이터를 호풀해야함
         model_instance.save() #모델에 데이터를 저장한다.
 
-        return render(request,'accountapp/hello_world.html', context={'model_instance': model_instance})
+        data_list = NewModel.objects.all() #이 모델의 모든 오브젝트드을 가지고 오겠다.
+
+
+        return render(request,'accountapp/hello_world.html', context={'data_list': data_list})
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!'})
+        data_list = NewModel.objects.all()  # 이 모델의 모든 오브젝트드을 가지고 오겠다.
+
+        return render(request, 'accountapp/hello_world.html', context={'data_list': data_list})
 #어떠한 주소로 들어갔을떄 이것을 볼수 있는지 확인
