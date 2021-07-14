7613154503efa8dd93a2679351ca19ddb9ecc0aa
airhac <oooh32141@gmail.com>
2021-07-14 15:16:34 +0900

    7/14 new commit 2 1. create.html 생성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -6,5 +6,5 @@ app_name  = 'accountapp'
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
-    path('create/', AccountCreateView.as_view(), name='create'), #클래스를 정상적으로 연동하기 위한 as_view()
+    path('create/', AccountCreateView.as_view(), name='create'), #클래스를 정상적으로 연동하기 위한 as_view()#routing을 해준것이다
 ]#어디로 ~하면 어디로 돌려 줄것인지
