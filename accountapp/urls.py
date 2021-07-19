8291b5552c09594ba940fe39baf1b455cb58cc05
airhac <oooh32141@gmail.com>
2021-07-20 00:08:54 +0900

    7/16 못 들었던 수업 진도 1. django bootstrap 사용 2. login,logout화면 생성

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name  = 'accountapp'
 
@@ -11,5 +11,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', AccountCreateView.as_view(), name='create'),
     #클래스를 정상적으로 연동하기 위한 as_view()#routing을 해준것이다
-
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
 ]#어디로 ~하면 어디로 돌려 줄것인지
--- gw_odh_project1/settings.py
+++ gw_odh_project1/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp'
 ]
 
@@ -148,5 +149,5 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = reverse_lazy('accounts:hello_world')
-LOGOUT_REDIRECT_URL = reverse_lazy('accounts:login')
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
