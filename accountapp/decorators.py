b369f84e382139a28b54063053659aab15596314
airhac <oooh32141@gmail.com>
2021-07-23 09:24:48 +0900

    7/23 commit 1 1. decorators 함수 추가   사용자 권한 확인 filttering

---
+++ accountapp/decorators.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponseForbidden
+
+
+def account_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        target_user = User.objects.get(pk=kwargs['pk']) #User의 조건을 부여해야함
+        if target_user == request.uesr:
+            return func(request, *args, **kwargs)
+        else:
+            return HttpResponseForbidden()
+    return decorated
