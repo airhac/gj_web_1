c2f624abdcf231448e64fbcfdbbfbd9b5bbd43db
airhac <oooh32141@gmail.com>
2021-07-27 10:47:34 +0900

    7/27 commit 2 1. profile보이기

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,2 +1,9 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
+
+app_name = 'profileapp'
+
 urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]#어디로 ~하면 어디로 돌려 줄것인지
--- profileapp/views.py
+++ profileapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
+
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name =  'profileapp/create.html'
+
+    def form_valid(self, form):
+        #이미지 파일  검증하고 나서 이후에 실행되는 함수
+        form.instance.user = self.request.user
+        #인스턴스의 user와 용청하는 user를 확인하는 방법
+        return super.form_valid(form)#부모의 form을 그냥 가져와서 사용하는 것
