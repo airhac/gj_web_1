8766797b7f3ba853e29cc54a89b9026ec20b8e7a
airhac <oooh32141@gmail.com>
2021-08-03 10:15:02 +0900

    8/03 commit 2 1. Model Form 생성, model 생성 및 적용

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
+    #다중 연결을 해주어야한다. 다른 테이블과 연결 해준다. 작성자 미상의 글로 남도록 해준다.
+    #누군가 탈퇴를 한 상황에
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to="article/", null=True)
+    # 관련한 이미지가 쌀인다.
+    content = models.TextField(null=True)
+    #장문이 될수 있는 경우 사용한다.
+
+    created_at = models.DateField(auto_now_add=True, null=True)
