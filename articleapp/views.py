ee64b6e977c0b5c9e8e64ecbf4e588f1f4eec537
airhac <oooh32141@gmail.com>
2021-08-04 15:43:51 +0900

    8/04 commit 2 1. Article delete, updateview 만들기

--- articleapp/views.py
+++ articleapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
@@ -17,9 +17,25 @@ class ArticleCreateView(CreateView):
         form.instance.writer = self.request.user
         return super().form_valid(form)
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
         #self.object target article과 동일
 class ArticleDetailView(DetailView):
     model = Article
-    context_object_name = 'target_object'
+    context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    success_url = reverse_lazy('articleapp:list')
+    template_name =  'articleapp/delete.html'
