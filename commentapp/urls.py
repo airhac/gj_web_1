82ba6227230955bc28a0e0bc2fcd65e2d5e29c89
airhac <oooh32141@gmail.com>
2021-08-11 14:42:38 +0900

    8/11 commit 1 1. deleteview 만들기

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from commentapp.views import CommentCreationView
+from commentapp.views import CommentCreationView, CommentDeleteView
 
 app_name = 'commentapp'
 
 urlpatterns =[
-    path('create/', CommentCreationView.as_view(), name='create')
+    path('create/', CommentCreationView.as_view(), name='create'),
+    path('delete/<int:pk>', CommentDeleteView.as_view(), name='delete')
 ]
--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,5 @@
+from _ast import Delete
+
 from django.shortcuts import render
 
 # Create your views here.
@@ -12,6 +14,21 @@ class CommentCreationView(CreateView):
     model = Comment
     form_class = CommentCreationForm
     template_name = 'commentapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.writer = self.request.user
+        form.instance.article_id = self.request.POST.get('article_pk')
+        #form의 객체에 바로 접근해서 데이터 베이스에 넣어 줍니다.
+        #article의 객체를 할당하는 방법 =>db로 접근 하는 방법
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+class CommentDeleteView(Delete):
+    model = Comment
+    context_object_name = 'target_comment'
+    template_name = 'commentapp/delete.html'
 
     def get_success_url(self):
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
