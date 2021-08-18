3758bf9fe6dfd06890f2caa1a15dc9d3263cb576
airhac <oooh32141@gmail.com>
2021-08-18 15:06:23 +0900

    8/18 commit 1 1. article에서 project를 구분할 수 있는 foreign key 생성 2. 그 projrct를 선택하는 form 생성

--- articleapp/models.py
+++ articleapp/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 
 # Create your models here.
+from projectapp.models import Project
+
+
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True)
     #다중 연결을 해주어야한다. 다른 테이블과 연결 해준다. 작성자 미상의 글로 남도록 해준다.
     #누군가 탈퇴를 한 상황에
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, related_name='article', null=True)
     title = models.CharField(max_length=200, null=True)
     image = models.ImageField(upload_to="article/", null=True)
     # 관련한 이미지가 쌀인다.
