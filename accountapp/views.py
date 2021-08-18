65dd362085171c173fcb494335e027be8bc4b0c4
airhac <oooh32141@gmail.com>
2021-08-18 16:00:11 +0900

    8/18 commit 1 1. 생성한 article들을 마이페이지와 project페이지에서 볼수 있도록 해줍니다.

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,12 +6,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic.list import MultipleObjectMixin
 
 from accountapp.decorators import account_ownership_required
 from accountapp.models import NewModel
 from accountapp.forms import AccountCreationForm
-
-
+from articleapp.models import Article
 
 
 @login_required(login_url=reverse_lazy('accountapp:login'))
@@ -38,11 +38,13 @@ class AccountCreateView(CreateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
 
-class AccountDetailView(DetailView):
+class AccountDetailView(DetailView, MultipleObjectMixin):
     model = User
     context_object_name = 'target_user' # target_user는 어떠한 키값이든지 간에 다 받는 변수의 이름
     template_name = 'accountapp/detail.html'
-
+    def get_context_data(self, **kwargs):
+        article_list = Article.objects.filter(writer=self.object)
+        return super().get_context_data(object_list=article_list,**kwargs)
 has_ownership = [login_required, account_ownership_required]
 
 
