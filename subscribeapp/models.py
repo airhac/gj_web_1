fe65117c1129d2111e8ca594fc055b22505f4e1b
airhac <oooh32141@gmail.com>
2021-08-20 15:06:20 +0900

    8/20 commit 1 1. 구독하기 위한 model생성 2. 구족 버튼 만들어 처리하기

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscribe', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='subscribe', null=False)
+    class Meta:
+        unique_together = ['user','project']
--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,4 +1,8 @@
-app_name = 'subscribeapp'
-url_patterns = [
+from django.urls import path
 
+from subscribeapp.views import SubscriptionView
+
+app_name = 'subscribeapp'
+urlpatterns=[
+    path('subscribe/<int:pk>', SubscriptionView.as_view(), name='subscribe')
 ]
